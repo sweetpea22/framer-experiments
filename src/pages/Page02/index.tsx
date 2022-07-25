1cfc20d8c1ccca217714f8826a0c056e7bdc731c
sweetpea22 <cindychau24@me.com>
2022-07-25 19:43:38 -0400

    First test :)

--- src/pages/Page02/index.tsx
+++ src/pages/Page02/index.tsx
@@ -16,6 +16,12 @@ export const Page02 = () => {
       </div>
       <div className='content'>
         <p>Horus is a flexible, modular library that  allows developers to easily query and broadcast to multiple chains through a single interface. By normalizing data returned from the different chains, it simplifies the development process.</p>
+        <div className='second'>
+         <p>Address the painpoints brought about by trusted systems and enable peer-to-peer transactions by removing themselves out of the equation.</p>
+         <p>Or reincarnate intermediation and lose the benefits of consensus-based networks</p>
+         <p>The choice is yours.</p>
+
+        </div>
       </div>
     </div>
   )
