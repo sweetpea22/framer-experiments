2d6b8136ea597feb9193386cbb9a4fd42fa6dd6b
sweetpea22 <cindychau24@me.com>
2022-07-25 15:54:58 -0400

    Set up

--- src/pages/Page01/index.tsx
+++ src/pages/Page01/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import {motion, useScroll} from 'framer-motion';
+import './index.scss';
 
 export const Page01 = () => {
+
+  const s2Ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: s2Ref,
+    offset: ["end end", "start end"]
+  });
+
   return (
-    <div>Page01</div>
+    <main>
+      <div className='wrapper'>
+      <div className='homepage'>
+        <div className='s1'>
+          <h1>Hello</h1>  
+        </div>
+        <div className='s2'></div>
+      </div>
+      </div>
+    </main>
   )
 }
